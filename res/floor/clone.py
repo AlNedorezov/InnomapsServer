ca90c88953412fdd83d342dfcdc954fdf475cca5
luckychess <toobwn@gmail.com>
2016-04-07 16:28:40 +0300

    Download xml's from github script.

---
+++ res/floor/clone.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import urllib.request
+
+url = 'https://raw.githubusercontent.com/luckychess/innomapsserver/master/res/floor/'
+
+for i in range(1, 6):
+	dlurl = url + str(i) + ".xml"
+	savename = str(i) + ".xml"
+	urllib.request.urlretrieve (dlurl, savename)
