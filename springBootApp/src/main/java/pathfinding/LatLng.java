117a8cc6d7c0d2b05d23ddc7ae2cbbe6aae1501e
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-08 14:11:32 +0300

    Added hashcode(), equals() and toString() methods to LatLng class from pathfinding package

--- springBootApp/src/main/java/pathfinding/LatLng.java
+++ springBootApp/src/main/java/pathfinding/LatLng.java
@@ -20,4 +20,29 @@ public class LatLng {
     public double getLongitude() {
         return longitude;
     }
+
+    public int hashCode() {
+        boolean var1 = true;
+        byte var2 = 1;
+        long var3 = Double.doubleToLongBits(this.latitude);
+        int var5 = 31 * var2 + (int) (var3 ^ var3 >>> 32);
+        var3 = Double.doubleToLongBits(this.longitude);
+        var5 = 31 * var5 + (int) (var3 ^ var3 >>> 32);
+        return var5;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        } else if (!(o instanceof LatLng)) {
+            return false;
+        } else {
+            LatLng var2 = (LatLng) o;
+            return Double.doubleToLongBits(this.latitude) == Double.doubleToLongBits(var2.latitude) && Double.doubleToLongBits(this.longitude) == Double.doubleToLongBits(var2.longitude);
+        }
+    }
+
+    public String toString() {
+        return "coordinates: (" + this.latitude + "," + this.longitude + ")";
+    }
 }
