4b40b2eb800a1a42d4dcd78010797e0a8b123c53
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-11 17:25:49 +0300

    Updated clone.py
    Change is necessary due to the repository transfer

--- res/floor/clone.py
+++ res/floor/clone.py
@@ -2,7 +2,7 @@
 
 import urllib.request
 
-url = 'https://raw.githubusercontent.com/luckychess/innomapsserver/master/res/floor/'
+url = 'https://raw.githubusercontent.com/AlNedorezov/innomapsserver/master/res/floor/'
 
 for i in range(1, 7):
 	dlurl = url + str(i) + ".xml"
