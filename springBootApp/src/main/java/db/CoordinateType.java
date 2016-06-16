927112b9a24f94a64fe3737c799b83f8770759f9
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-16 22:11:33 +0300

    RoomType -> CoordinateType

--- springBootApp/src/main/java/db/CoordinateType.java
+++ springBootApp/src/main/java/db/CoordinateType.java
@@ -6,20 +6,20 @@ import com.j256.ormlite.table.DatabaseTable;
 /**
  * Created by alnedorezov on 6/16/16.
  */
-@DatabaseTable(tableName = "Room_types")
-public class RoomType {
+@DatabaseTable(tableName = "Coordinate_types")
+public class CoordinateType {
     @DatabaseField(generatedId = true, unique = true)
     private int id;
     @DatabaseField(unique = true)
     private String name;
 
-    public RoomType(int id, String name) {
+    public CoordinateType(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
     // For deserialization with Jackson
-    public RoomType() {
+    public CoordinateType() {
         // all persisted classes must define a no-arg constructor with at least package visibility
     }
 
