503e06fa5817efcc7c105d191cabe2936220dcb6
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-17 03:59:11 +0300

    Added GraphElementTypes to JGraphTWrapper

--- springBootApp/src/main/java/pathfinding/JGraphTWrapper.java
+++ springBootApp/src/main/java/pathfinding/JGraphTWrapper.java
@@ -228,7 +228,7 @@ public class JGraphTWrapper {
         return v;
     }
 
-    private static double haversine(double lat1, double lon1, double lat2, double lon2) {
+    public static double haversine(double lat1, double lon1, double lat2, double lon2) {
         double dLat = Math.toRadians(lat2 - lat1);
         double dLon = Math.toRadians(lon2 - lon1);
         lat1 = Math.toRadians(lat1);
