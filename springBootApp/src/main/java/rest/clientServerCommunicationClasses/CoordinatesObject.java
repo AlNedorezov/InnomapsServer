e6e74823c38792d80120848f701d59404f0591c0
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-21 08:58:20 +0300

    Implemented CoordinatesObject class

--- springBootApp/src/main/java/rest/clientServerCommunicationClasses/CoordinatesObject.java
+++ springBootApp/src/main/java/rest/clientServerCommunicationClasses/CoordinatesObject.java
@@ -1,7 +1,36 @@
 package rest.clientServerCommunicationClasses;
 
+import db.Coordinate;
+
+import java.util.List;
+
 /**
  * Created by alnedorezov on 6/21/16.
  */
 public class CoordinatesObject {
+    private List<Coordinate> coordinates;
+
+    public CoordinatesObject(List<Coordinate> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public void addCoordinate(Coordinate coordinate) {
+        this.coordinates.add(coordinate);
+    }
+
+    public void setCoordinate(int index, Coordinate coordinate) {
+        this.coordinates.set(index, coordinate);
+    }
+
+    public void getCoordinate(int index) {
+        this.coordinates.get(index);
+    }
+
+    public void removeCoordinate(int index) {
+        this.coordinates.remove(index);
+    }
+
+    public List<Coordinate> getCoordinates() {
+        return coordinates;
+    }
 }
