950b6d4a8b8b4441fe59b5a151931d7261127bf8
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-21 09:54:47 +0300

    IN-43. Implemented controller for DELETE requests for coordinates

--- springBootApp/src/main/java/rest/CoordinatesController.java
+++ springBootApp/src/main/java/rest/CoordinatesController.java
@@ -1,7 +1,6 @@
 package rest;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
-import com.j256.ormlite.stmt.QueryBuilder;
 import db.Coordinate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,11 +37,37 @@ public class CoordinatesController {
     }
 
     @RequestMapping(value = "/resources/coordinate", method = RequestMethod.POST)
-    public String logs(@RequestParam(value = "id", defaultValue = "-1") int id, @RequestParam(value = "latitude", defaultValue = "0") double latitude,
-                       @RequestParam(value = "longitude", defaultValue = "0") double longitude, @RequestParam(value = "floor", defaultValue = "1") int floor,
-                       @RequestParam(value = "typeid", defaultValue = "-2") int type_id, @RequestParam(value = "name", defaultValue = "") String name,
-                       @RequestParam(value = "description", defaultValue = "") String description) throws SQLException {
+    public String logs(@RequestParam(value = "id", defaultValue = "-1") int id, @RequestParam(value = "latitude", defaultValue = "-2") double latitude,
+                       @RequestParam(value = "longitude", defaultValue = "-3") double longitude, @RequestParam(value = "floor", defaultValue = "1") int floor,
+                       @RequestParam(value = "typeid", defaultValue = "-4") int type_id, @RequestParam(value = "name", defaultValue = "!~NO_NAME") String name,
+                       @RequestParam(value = "description", defaultValue = "!~NO_DESCRIPTION") String description) throws SQLException {
 
+        JdbcConnectionSource connectionSource = new JdbcConnectionSource(a.DATABASE_URL, "h2", "");
+        a.setupDatabase(connectionSource, false);
+
+        if (latitude == -2 && longitude == -3 && floor == 1 && type_id == -4 &&
+                name.equals("!~NO_NAME") && description.equals("!~NO_DESCRIPTION")) {
+            // Deleting a coordinate
+            System.out.println("Received POST request: delete coordinate with id=" + id);
+            if (id == -1)
+                return "-1. Wrong parameters.\n";
+            else if (!a.coordinateDao.idExists(id)) {
+                connectionSource.close();
+                return "-1. There is no such coordinate.\n";
+            } else {
+                a.coordinateDao.deleteById(id);
+                connectionSource.close();
+                return "0. Coordinate with id=" + id + " was successfully deleted.\n";
+            }
+        }
+        else {
+            if (id == -1) {
+                // Creating a coordinate
+            }
+            else {
+                // Updating a coordinate
+            }
+        }
 
         return "УБРАТЬ КОГДА ДОДЕЛАЮ!";
     }
