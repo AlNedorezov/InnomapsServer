42edc76b9882521986d0ae57d7b70b446f06cda4
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-21 16:22:35 +0300

    Implemented EdgeTypesObject class

---
+++ springBootApp/src/main/java/rest/clientServerCommunicationClasses/EdgeTypesObject.java
@@ -0,0 +1,37 @@
+package rest.clientServerCommunicationClasses;
+
+import db.EdgeType;
+
+import java.util.List;
+
+/**
+ * Created by alnedorezov on 6/21/16.
+ */
+
+public class EdgeTypesObject {
+    private List<EdgeType> edgeTypes;
+
+    public EdgeTypesObject(List<EdgeType> edgeTypes) {
+        this.edgeTypes = edgeTypes;
+    }
+
+    public void addEdgeType(EdgeType edgeType) {
+        this.edgeTypes.add(edgeType);
+    }
+
+    public void setEdgeType(int index, EdgeType edgeType) {
+        this.edgeTypes.set(index, edgeType);
+    }
+
+    public void getEdgeType(int index) {
+        this.edgeTypes.get(index);
+    }
+
+    public void removeEdgeType(int index) {
+        this.edgeTypes.remove(index);
+    }
+
+    public List<EdgeType> getEdgetypes() {
+        return edgeTypes;
+    }
+}
