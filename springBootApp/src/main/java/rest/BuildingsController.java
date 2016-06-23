3461f6c237fb8e27b84d0002f3e08f3edc6d4ef5
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-23 15:34:39 +0300

    IN-46. Implemented controller for GET requests for Building

--- springBootApp/src/main/java/rest/BuildingsController.java
+++ springBootApp/src/main/java/rest/BuildingsController.java
@@ -1,7 +1,41 @@
 package rest;
 
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.stmt.QueryBuilder;
+import db.Building;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import rest.clientServerCommunicationClasses.BuildingsObject;
+
+import java.sql.SQLException;
+
 /**
  * Created by alnedorezov on 6/23/16.
  */
+
+@RestController
 public class BuildingsController {
+    Application a = new Application();
+
+    @RequestMapping("/resources/buildings")
+    public BuildingsObject buildings() throws SQLException {
+        JdbcConnectionSource connectionSource = new JdbcConnectionSource(Application.DATABASE_URL,
+                Application.DATABASE_USERNAME, Application.DATABASE_PASSWORD);
+        a.setupDatabase(connectionSource, false);
+        BuildingsObject buildings1 = new BuildingsObject(a.buildingDao.queryForAll());
+        connectionSource.close();
+        return buildings1;
+    }
+
+    @RequestMapping("/resources/building")
+    public Building building(@RequestParam(value = "id", defaultValue = "-1") int id) throws SQLException {
+        JdbcConnectionSource connectionSource = new JdbcConnectionSource(Application.DATABASE_URL,
+                Application.DATABASE_USERNAME, Application.DATABASE_PASSWORD);
+        a.setupDatabase(connectionSource, false);
+        Building building1 = a.buildingDao.queryForId(id);
+        connectionSource.close();
+        return building1;
+    }
 }
