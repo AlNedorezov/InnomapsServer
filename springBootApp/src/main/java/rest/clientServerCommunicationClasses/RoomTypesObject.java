0cd90596fc64f2ec680de5f387e2d0aff47e334d
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-23 08:03:34 +0300

    IN-46. Implemented RoomTypesObject

---
+++ springBootApp/src/main/java/rest/clientServerCommunicationClasses/RoomTypesObject.java
@@ -0,0 +1,36 @@
+package rest.clientServerCommunicationClasses;
+
+import db.RoomType;
+
+import java.util.List;
+
+/**
+ * Created by alnedorezov on 6/23/16.
+ */
+public class RoomTypesObject {
+    private List<RoomType> roomTypes;
+
+    public RoomTypesObject(List<RoomType> roomTypes) {
+        this.roomTypes = roomTypes;
+    }
+
+    public void addRoomType(RoomType roomType) {
+        this.roomTypes.add(roomType);
+    }
+
+    public void setRoomType(int index, RoomType roomType) {
+        this.roomTypes.set(index, roomType);
+    }
+
+    public void getRoomType(int index) {
+        this.roomTypes.get(index);
+    }
+
+    public void removeRoomType(int index) {
+        this.roomTypes.remove(index);
+    }
+
+    public List<RoomType> getRoomtypes() {
+        return roomTypes;
+    }
+}
