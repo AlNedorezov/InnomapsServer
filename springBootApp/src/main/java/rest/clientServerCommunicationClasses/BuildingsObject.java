a66b23bd49b32c993af0a5b4a9c84778f00ade8b
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-23 15:13:33 +0300

    IN-48. Implemented BuildingsObject class

---
+++ springBootApp/src/main/java/rest/clientServerCommunicationClasses/BuildingsObject.java
@@ -0,0 +1,36 @@
+package rest.clientServerCommunicationClasses;
+
+import db.Building;
+
+import java.util.List;
+
+/**
+ * Created by alnedorezov on 6/23/16.
+ */
+public class BuildingsObject {
+    private List<Building> buildings;
+
+    public BuildingsObject(List<Building> buildings) {
+        this.buildings = buildings;
+    }
+
+    public void addBuilding(Building building) {
+        this.buildings.add(building);
+    }
+
+    public void setBuilding(int index, Building building) {
+        this.buildings.set(index, building);
+    }
+
+    public void getBuilding(int index) {
+        this.buildings.get(index);
+    }
+
+    public void removeBuilding(int index) {
+        this.buildings.remove(index);
+    }
+
+    public List<Building> getBuildings() {
+        return buildings;
+    }
+}
