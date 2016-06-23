6223e08a121b77259f7321503157b492aa4aa3d2
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-23 20:56:17 +0300

    IN-50. Implemented PhotosObject class

---
+++ springBootApp/src/main/java/rest/clientServerCommunicationClasses/PhotosObject.java
@@ -0,0 +1,36 @@
+package rest.clientServerCommunicationClasses;
+
+import db.Photo;
+
+import java.util.List;
+
+/**
+ * Created by alnedorezov on 6/23/16.
+ */
+public class PhotosObject {
+    private List<Photo> photos;
+
+    public PhotosObject(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    public void addPhoto(Photo photo) {
+        this.photos.add(photo);
+    }
+
+    public void setPhoto(int index, Photo photo) {
+        this.photos.set(index, photo);
+    }
+
+    public void getPhoto(int index) {
+        this.photos.get(index);
+    }
+
+    public void removePhoto(int index) {
+        this.photos.remove(index);
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+}
