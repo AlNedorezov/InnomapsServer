f10baf6f241ff39424c355608a8a869f771f9258
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-23 22:01:19 +0300

    IN-51, IN-52. Implemented BuildingPhotosObject & RoomPhotosObject classes

---
+++ springBootApp/src/main/java/rest/clientServerCommunicationClasses/BuildingPhotosObject.java
@@ -0,0 +1,36 @@
+package rest.clientServerCommunicationClasses;
+
+import db.BuildingPhoto;
+
+import java.util.List;
+
+/**
+ * Created by alnedorezov on 6/23/16.
+ */
+public class BuildingPhotosObject {
+    private List<BuildingPhoto> buildingPhotos;
+
+    public BuildingPhotosObject(List<BuildingPhoto> buildingPhotos) {
+        this.buildingPhotos = buildingPhotos;
+    }
+
+    public void addBuildingPhoto(BuildingPhoto buildingPhoto) {
+        this.buildingPhotos.add(buildingPhoto);
+    }
+
+    public void setBuildingPhoto(int index, BuildingPhoto buildingPhoto) {
+        this.buildingPhotos.set(index, buildingPhoto);
+    }
+
+    public void getBuildingPhoto(int index) {
+        this.buildingPhotos.get(index);
+    }
+
+    public void removeBuildingPhoto(int index) {
+        this.buildingPhotos.remove(index);
+    }
+
+    public List<BuildingPhoto> getUserroles() {
+        return buildingPhotos;
+    }
+}
---
+++ springBootApp/src/main/java/rest/clientServerCommunicationClasses/RoomPhotosObject.java
@@ -0,0 +1,36 @@
+package rest.clientServerCommunicationClasses;
+
+import db.RoomPhoto;
+
+import java.util.List;
+
+/**
+ * Created by alnedorezov on 6/23/16.
+ */
+public class RoomPhotosObject {
+    private List<RoomPhoto> roomPhotos;
+
+    public RoomPhotosObject(List<RoomPhoto> roomPhotos) {
+        this.roomPhotos = roomPhotos;
+    }
+
+    public void addRoomPhoto(RoomPhoto roomPhoto) {
+        this.roomPhotos.add(roomPhoto);
+    }
+
+    public void setRoomPhoto(int index, RoomPhoto roomPhoto) {
+        this.roomPhotos.set(index, roomPhoto);
+    }
+
+    public void getRoomPhoto(int index) {
+        this.roomPhotos.get(index);
+    }
+
+    public void removeRoomPhoto(int index) {
+        this.roomPhotos.remove(index);
+    }
+
+    public List<RoomPhoto> getUserroles() {
+        return roomPhotos;
+    }
+}
