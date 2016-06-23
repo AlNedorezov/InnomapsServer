a1a5ca9a1ccdc0f2f884ede5c9288fb5f70ae259
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-23 09:41:25 +0300

    IN-47. Implemented db.StreetsObject.java class

---
+++ springBootApp/src/main/java/rest/clientServerCommunicationClasses/StreetsObject.java
@@ -0,0 +1,36 @@
+package rest.clientServerCommunicationClasses;
+
+import db.Street;
+
+import java.util.List;
+
+/**
+ * Created by alnedorezov on 6/23/16.
+ */
+public class StreetsObject {
+    private List<Street> streets;
+
+    public StreetsObject(List<Street> streets) {
+        this.streets = streets;
+    }
+
+    public void addStreet(Street street) {
+        this.streets.add(street);
+    }
+
+    public void setStreet(int index, Street street) {
+        this.streets.set(index, street);
+    }
+
+    public void getStreet(int index) {
+        this.streets.get(index);
+    }
+
+    public void removeStreet(int index) {
+        this.streets.remove(index);
+    }
+
+    public List<Street> getStreets() {
+        return streets;
+    }
+}
