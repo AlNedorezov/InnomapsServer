e5ee95c91f745a28c929ba23ea0a7e656b0e2893
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-06-24 12:51:54 +0300

    IN-53. Implemented EventCreatorsObject class

---
+++ springBootApp/src/main/java/rest/clientServerCommunicationClasses/EventCreatorsObject.java
@@ -0,0 +1,36 @@
+package rest.clientServerCommunicationClasses;
+
+import db.EventCreator;
+
+import java.util.List;
+
+/**
+ * Created by alnedorezov on 6/24/16.
+ */
+public class EventCreatorsObject {
+    private List<EventCreator> eventCreators;
+
+    public EventCreatorsObject(List<EventCreator> eventCreators) {
+        this.eventCreators = eventCreators;
+    }
+
+    public void addEventCreator(EventCreator eventCreator) {
+        this.eventCreators.add(eventCreator);
+    }
+
+    public void setEventCreator(int index, EventCreator eventCreator) {
+        this.eventCreators.set(index, eventCreator);
+    }
+
+    public void getEventCreator(int index) {
+        this.eventCreators.get(index);
+    }
+
+    public void removeEventCreator(int index) {
+        this.eventCreators.remove(index);
+    }
+
+    public List<EventCreator> getEventcreators() {
+        return eventCreators;
+    }
+}
