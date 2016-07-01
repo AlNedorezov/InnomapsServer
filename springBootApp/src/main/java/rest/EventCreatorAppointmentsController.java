3d69abb8ec68b5f0e01b883374a4821e464e843f
Aleksandr Nedorezov <Al73rus@gmail.com>
2016-07-01 10:14:30 +0300

    IN-76 Implemented controller for the EventCreatorAppointment class offering CRUD capabilities

--- springBootApp/src/main/java/rest/EventCreatorAppointmentsController.java
+++ springBootApp/src/main/java/rest/EventCreatorAppointmentsController.java
@@ -42,4 +42,54 @@ public class EventCreatorAppointmentsController {
         connectionSource.close();
         return eventCreatorAppointment1;
     }
+
+    @RequestMapping(value = "/resources/eventcreatorappointment", method = RequestMethod.POST)
+    public String logs(@RequestParam(value = "eventid", defaultValue = "-1") int event_id,
+                       @RequestParam(value = "eventcreatorid", defaultValue = "-2") int event_creator_id) throws SQLException {
+        JdbcConnectionSource connectionSource = new JdbcConnectionSource(Application.DATABASE_URL,
+                Application.DATABASE_USERNAME, Application.DATABASE_PASSWORD);
+        a.setupDatabase(connectionSource, false);
+        QueryBuilder<EventCreatorAppointment, Integer> qb = a.eventCreatorAppointmentDao.queryBuilder();
+        qb.where().eq("event_id", event_id).and().eq("event_creator_id", event_creator_id);
+        PreparedQuery<EventCreatorAppointment> pc = qb.prepare();
+        List<EventCreatorAppointment> eventCreatorAppointments = a.eventCreatorAppointmentDao.query(pc);
+        String action;
+        if (eventCreatorAppointments.size() > 0)
+            action = "delete";
+        else
+            action = "add";
+
+        if (event_id == -1 || event_creator_id == -2) {
+            connectionSource.close();
+            return "-1. Wrong parameters.";
+        } else {
+            String errorMessageOnAdd = checkIfEventCreatorAppointmentCanBeAdded(event_id, event_creator_id);
+            if (!errorMessageOnAdd.equals("")) {
+                connectionSource.close();
+                return "-1. " + errorMessageOnAdd;
+            } else if (action.equals("add")) {
+                System.out.println("Received POST request: event creator with id=" + event_creator_id + " was assigned to event with id=" + event_id);
+                a.eventCreatorAppointmentDao.create(new EventCreatorAppointment(event_id, event_creator_id));
+                connectionSource.close();
+                return "0. Event creator with id=" + event_creator_id + " was assigned to event with id=" + event_id + "\n";
+            } else { // if action = delete
+                System.out.println("Received POST request: event creator with id=" + event_creator_id + " was unassigned from event with id=" + event_id);
+                DeleteBuilder<EventCreatorAppointment, Integer> db = a.eventCreatorAppointmentDao.deleteBuilder();
+                db.where().eq("event_id", event_id).and().eq("event_creator_id", event_creator_id);
+                PreparedDelete<EventCreatorAppointment> preparedDelete = db.prepare();
+                a.eventCreatorAppointmentDao.delete(preparedDelete);
+                connectionSource.close();
+                return "0. Event creator with id=" + event_creator_id + " was unassigned from event with id=" + event_id + "\n";
+            }
+        }
+    }
+
+    private String checkIfEventCreatorAppointmentCanBeAdded(int event_id, int event_creator_id) throws SQLException {
+        String errorMessage = "";
+
+        errorMessage += CommonFunctions.checkIfEventExist(event_id);
+        errorMessage += CommonFunctions.checkIfEventCreatorExist(event_creator_id);
+
+        return errorMessage;
+    }
 }
